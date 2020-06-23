dbbe77294f0959b2149672b7b7783826c6898560
jalbas2000 <jalbas2000@yahoo.com>
2020-06-23 17:50:57 +0400

    End of section 19

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
+import { JwtInterceptor } from './core/interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { CoreModule } from './core/core.module';
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
-    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}
   ],
   bootstrap: [AppComponent]
 })
---
+++ client/src/app/checkout/checkout-address/checkout-address.component.ts
@@ -0,0 +1,29 @@
+import { ToastrService } from 'ngx-toastr';
+import { AccountService } from './../../account/account.service';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkout-address',
+  templateUrl: './checkout-address.component.html',
+  styleUrls: ['./checkout-address.component.scss']
+})
+export class CheckoutAddressComponent implements OnInit {
+  @Input() checkoutForm: FormGroup;
+
+  constructor(private accountService: AccountService, private toaster: ToastrService) { }
+
+  ngOnInit(): void {
+  }
+
+  saveUserAddress() {
+    this.accountService.updateUserAddress(this.checkoutForm.get('addressForm').value)
+      .subscribe(() => {
+        this.toaster.success('Address saved');
+      }, error => {
+        this.toaster.error(error.message);
+        console.log(error);
+      });
+  }
+
+}
